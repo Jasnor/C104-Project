8d11accced7ce965a51b2fcf772aa4d3c96a4da3
Jasnor <133242690+Jasnor@users.noreply.github.com>
2024-03-07 17:54:29 +0000

    Add files via upload

---
+++ PRO-104-Project-Image-main/Planets.py
@@ -0,0 +1,68 @@
+import cv2
+import numpy
+img = cv2.imread("solar-system.jpg")
+cv2.putText(img,
+        "Sun",
+        (20,300),
+        cv2.FONT_HERSHEY_COMPLEX,
+        0.5,
+        (255,255,255)
+        )
+cv2.putText(img,
+        "Mercury",
+        (110,220),
+        cv2.FONT_HERSHEY_COMPLEX,
+        0.5,
+        (255,255,255)
+        )
+cv2.putText(img,
+        "Venus",
+        (190,220),
+        cv2.FONT_HERSHEY_COMPLEX,
+        0.5,
+        (255,255,255)
+        )
+cv2.putText(img,
+        "Earth",
+        (280,220),
+        cv2.FONT_HERSHEY_COMPLEX,
+        0.5,
+        (255,255,255)
+        )
+cv2.putText(img,
+        "Mars",
+        (380,220),
+        cv2.FONT_HERSHEY_COMPLEX,
+        0.5,
+        (255,255,255)
+        )
+cv2.putText(img,
+        "Jupiter",
+        (530,220),
+        cv2.FONT_HERSHEY_COMPLEX,
+        0.5,
+        (31)
+        )
+cv2.putText(img,
+        "Saturn",
+        (750,220),
+        cv2.FONT_HERSHEY_COMPLEX,
+        0.5,
+        (255,255,255)
+        )
+cv2.putText(img,
+        "Uranus",
+        (950,220),
+        cv2.FONT_HERSHEY_COMPLEX,
+        0.5,
+        (255,255,255)
+        )
+cv2.putText(img,
+        "Neptune",
+        (1100,220),
+        cv2.FONT_HERSHEY_COMPLEX,
+        0.5,
+        (255,255,255)
+        )
+cv2.imshow("output",img)
+cv2.waitKey(0)
